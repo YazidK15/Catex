f21fb530392bfa34ed70bdac74e72ad570ee9cc6
Yazid <yaazid15@gmail.com>
2024-08-16 17:01:24 +0200

    Ajout de la page à propos

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const location = [50.749454498291016, 3.022520065307617];   
+    const map = L.map('map').setView(location, 14); 
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker(location).addTo(map)
+        .bindPopup('Cadex, France <br> 68 rue du vieil dieu, Comines')
+        .openPopup();
+});
