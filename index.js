67c305821b73b2601da0c77066906c80844a074b
Yazid <yaazid15@gmail.com>
2024-08-20 22:58:21 +0200

    Responsive

--- index.js
+++ index.js
@@ -72,12 +72,3 @@ function moveDivisor() {
 }
 
 
-    const cards = document.querySelectorAll('.col');
-
-    cards.forEach(card => {
-        card.addEventListener('click', function() {
-            // Toggle the hover class on click
-            this.classList.toggle('hover');
-        });
-    });
-
